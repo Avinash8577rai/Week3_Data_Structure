f8a77cea4712d9f221ee2cb3234d058c8343eb05
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-28 22:36:41 +0530

    here in this program we use single linkedlist

---
+++ src/day1linkedlist/singlelinkedlist/inventorymanagementsystem/Item.java
@@ -0,0 +1,18 @@
+package day1linkedlist.singlelinkedlist.inventorymanagementsystem;
+
+public class Item {
+
+        String itemName;
+        int itemID;
+        int quantity;
+        double price;
+        Item next;
+
+        public Item(String itemName, int itemID, int quantity, double price) {
+            this.itemName = itemName;
+            this.itemID = itemID;
+            this.quantity = quantity;
+            this.price = price;
+            this.next = null;
+        }
+    }
