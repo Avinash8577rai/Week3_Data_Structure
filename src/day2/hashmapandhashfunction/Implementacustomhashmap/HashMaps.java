9cffe0e0e80b9c80bec73ac8936f1dfab399431e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-30 13:09:41 +0530

    here in this program we use hash map and hashset problem 3

--- src/day2/hashmapandhashfunction/Implementacustomhashmap/HashMaps.java
+++ src/day2/hashmapandhashfunction/Implementacustomhashmap/HashMaps.java
@@ -1,4 +1,79 @@
 package day2.hashmapandhashfunction.Implementacustomhashmap;
-
+import java.util.*;
 public class HashMaps {
+    private static final int SIZE = 10;
+
+    // Create an array of LinkedLists (each element is a linked list of key-value pairs)
+    private LinkedList<Node>[] table;
+
+    // Constructor
+    public HashMaps() {
+        table = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    // Hash function to compute the index for a given key
+    private int hash(int key) {
+        return Integer.hashCode(key) % SIZE;
+    }
+
+    // Node to store key-value pairs
+    private static class Node {
+        int key;
+        int value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    // Insert key-value pair into the map
+    public void put(int key, int value) {
+        int index = hash(key);
+        LinkedList<Node> list = table[index];
+
+        // Check if the key already exists, if so, update the value
+        for (Node node : list) {
+            if (node.key == key) {
+                node.value = value;  // Update value
+                return;
+            }
+        }
+
+        // Otherwise, add a new node with the key-value pair
+        list.add(new Node(key, value));
+    }
+
+    // Retrieve the value for a given key
+    public int get(int key) {
+        int index = hash(key);
+        LinkedList<Node> list = table[index];
+
+        // Search for the key in the linked list
+        for (Node node : list) {
+            if (node.key == key) {
+                return node.value;  // Return the value if found
+            }
+        }
+
+        return -1;  // Return -1 if the key doesn't exist
+    }
+
+    // Remove the key-value pair from the map
+    public void remove(int key) {
+        int index = hash(key);
+        LinkedList<Node> list = table[index];
+
+        // Search and remove the node with the given key
+        for (Node node : list) {
+            if (node.key == key) {
+                list.remove(node);  // Remove the node
+                return;
+            }
+        }
+    }
+
 }
--- src/day2/hashmapandhashfunction/Implementacustomhashmap/Main.java
+++ src/day2/hashmapandhashfunction/Implementacustomhashmap/Main.java
@@ -1,4 +1,22 @@
 package day2.hashmapandhashfunction.Implementacustomhashmap;
 
 public class Main {
-}
+    public static void main(String[] args) {
+
+            HashMaps map = new HashMaps();
+
+            // Insert some key-value pairs
+            map.put(1, 10);
+            map.put(2, 20);
+            map.put(100, 1000);
+
+            // Retrieve and print values
+            System.out.println("Value for key 1: " + map.get(1));  // Output: 10
+            System.out.println("Value for key 2: " + map.get(2));  // Output: 20
+            System.out.println("Value for key 100: " + map.get(100));  // Output: 1000
+
+            // Remove a key-value pair
+            map.remove(2);
+            System.out.println("Value for key 2 after removal: " + map.get(2));  // Output: -1 (key 2 is removed)
+        }
+    }
