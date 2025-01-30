fbd5a59b0e468fcf35fa1b45c10a37579b6eb1cf
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-30 13:08:58 +0530

    here in this program we use hash map and hashset problem 2

--- src/day2/hashmapandhashfunction/findallsubarrayswithzerosum/Main.java
+++ src/day2/hashmapandhashfunction/findallsubarrayswithzerosum/Main.java
@@ -1,4 +1,13 @@
 package day2.hashmapandhashfunction.findallsubarrayswithzerosum;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 1, 3, 3, -6};
+        List<int[]> subarrays = SumOfSubarray.findZeroSumSubarrays(arr);
+
+        for (int[] subarray : subarrays) {
+            System.out.println("Subarray found from index " + subarray[0] + " to " + subarray[1]);
+        }
+    }
 }
+
--- src/day2/hashmapandhashfunction/findallsubarrayswithzerosum/SumOfSubarray.java
+++ src/day2/hashmapandhashfunction/findallsubarrayswithzerosum/SumOfSubarray.java
@@ -1,4 +1,32 @@
 package day2.hashmapandhashfunction.findallsubarrayswithzerosum;
-
+import java.util.*;
 public class SumOfSubarray {
-}
+        public static List<int[]> findZeroSumSubarrays(int[] arr) {
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            List<int[]> result = new ArrayList<>();
+            int sum = 0;
+
+            // Add base case: sum 0 at index -1 (for subarrays starting at index 0)
+            map.put(0, new ArrayList<>());
+            map.get(0).add(-1);
+
+            // Traverse array
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];  // Compute cumulative sum
+
+                // If sum has appeared before, subarrays exist
+                if (map.containsKey(sum)) {
+                    for (int start : map.get(sum)) {
+                        result.add(new int[]{start + 1, i}); // Store (start+1, i) as subarray indices
+                    }
+                }
+
+                // Store current sum's index
+                map.putIfAbsent(sum, new ArrayList<>());
+                map.get(sum).add(i);
+            }
+
+            return result;
+        }
+
+    }
