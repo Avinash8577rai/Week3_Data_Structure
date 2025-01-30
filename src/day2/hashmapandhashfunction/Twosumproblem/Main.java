ee661843a4b6003a48165b1609c871d2ae69e29e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-30 13:10:38 +0530

    here in this program we use hash map and hashset problem 4

--- src/day2/hashmapandhashfunction/Twosumproblem/Main.java
+++ src/day2/hashmapandhashfunction/Twosumproblem/Main.java
@@ -1,4 +1,16 @@
 package day2.hashmapandhashfunction.Twosumproblem;
 
 public class Main {
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = solution.twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found");
+        }
+    }
 }
--- src/day2/hashmapandhashfunction/Twosumproblem/TwoSum.java
+++ src/day2/hashmapandhashfunction/Twosumproblem/TwoSum.java
@@ -1,24 +1,24 @@
 package day2.hashmapandhashfunction.Twosumproblem;
-
+import java.util.*;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        // HashMap to store the number and its index
+
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Traverse the array
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];  // Calculate the complement
 
-            // Check if the complement exists in the map
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
             if (map.containsKey(complement)) {
-                // If it exists, return the indices of the complement and the current element
+
                 return new int[]{map.get(complement), i};
             }
 
-            // If not, store the current number and its index in the map
+
             map.put(nums[i], i);
         }
 
-        // Return an empty array if no solution is found
+
         return new int[]{};
+    }
 }
