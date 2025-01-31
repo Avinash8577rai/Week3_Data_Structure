03e12463bc3880d639027757d391a9def9577e9c
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-01-31 12:38:57 +0530

    here in this program we have to sort the marks of studnets using selection sort

--- src/Day3/Selectionsort/SortExamScores.java
+++ src/Day3/Selectionsort/SortExamScores.java
@@ -1,4 +1,35 @@
 package Day3.Selectionsort;
-
+import java.sql.SQLOutput;
+import java.util.*;
 public class SortExamScores {
+    public static void selectionSort(int arr[]){
+        System.out.print("Sorted Exam scores are: ");
+    for(int i=0; i< arr.length; i++){
+        System.out.print(arr[i]+ "  ");
+    }
+}
+            public static void main (String[] args){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter the number of students:");
+                int n = sc.nextInt();
+                int[] arr=new int[n];
+                for (int i=0; i<arr.length; i++){
+                    System.out.println(arr[i] + " Enter the marks of student: "+ (i+1));
+                    arr[i]=sc.nextInt();
+                }
+                for(int i=0; i<arr.length-1;i++){
+                    int s=i;
+                    for( int j=i+1; j<arr.length; j++) {
+                        if (arr[j] < arr[s]) {
+                            s = j;
+                        }
+                    }
+                            int temp= arr[s];
+                            arr[s]=arr[i];
+                            arr[i]=temp;
+                    }
+
+                selectionSort(arr);
+
+}
 }
