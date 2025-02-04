0955ee207602d31654edf5675eb1416a33f45a0e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-04 13:56:39 +0530

    Avinash added a program to  to read and write using a sytring builder and strig buffer and also input stream reader and file reader by given path

---
+++ src/Day4/Challengeproblem/Comparing.java
@@ -0,0 +1,38 @@
+package Day4.Challengeproblem;
+
+import java.io.*;
+public class Comparing {
+
+        public static void main(String[] args){
+            int iterations = 1_000_000;
+            String text = "hello";
+            long startTime = System.nanoTime();
+            StringBuffer stringBuffer = new StringBuffer();
+            for (int i = 0; i < iterations; i++) {
+                stringBuffer.append(text);
+            }
+            long endTime = System.nanoTime();
+            long stringBufferTime = endTime - startTime;
+            startTime = System.nanoTime();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < iterations; i++) {
+                stringBuilder.append(text);
+            }
+            endTime = System.nanoTime();
+            long stringBuilderTime = endTime - startTime;
+
+            System.out.println("Time taken by StringBuffer: " + stringBufferTime + " ns");
+            System.out.println("Time taken by StringBuilder: " + stringBuilderTime + " ns");
+            String filepath = "C:\\Users\\AVINASH KUMAR RAI\\Desktop\\capgemini training\\Week3\\src\\Day4\\Challengeproblem\\100mb-examplefile-com.txt";
+            int count = 0 ;
+            try(BufferedReader bf = new BufferedReader(new FileReader(filepath))){
+                String line ;
+                while((line=bf.readLine()) != null) {
+                    count++;
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            System.out.println("Total Count of words in 100mb file: " +count);
+        }
+}
