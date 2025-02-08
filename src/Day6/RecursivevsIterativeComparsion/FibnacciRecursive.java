f3307cb690ab4c77ac5896f55420c95f09bfca9b
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-08 16:04:15 +0530

    [Avinash] add. a program in which we have to check the iterrative and recursive process and calculating the time complexity

---
+++ src/Day6/RecursivevsIterativeComparsion/FibnacciRecursive.java
@@ -0,0 +1,9 @@
+package Day6.RecursivevsIterativeComparsion;
+
+public class FibnacciRecursive {
+    public static int fibonacciRecursive(int n) {
+        if (n <= 1) return n;
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+}
