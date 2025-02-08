4df73e2b9bf37520a619dff7359c146ca9d9c51a
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-08 15:40:11 +0530

    [Avinash] add. a program in which we have to check the iterrative and recursive process and calculating the time complexity

--- src/Day6/RecursivevsIterativeComparsion/FibnacciComparison.java
+++ src/Day6/RecursivevsIterativeComparsion/FibnacciComparison.java
@@ -1,4 +1,29 @@
 package Day6.RecursivevsIterativeComparsion;
 
 public class FibnacciComparison {
+    public static void main(String[] args) {
+
+        int[] testValues = {10, 30, 50};
+
+
+        for (int n : testValues) {
+
+            long startTimeRecursive = System.nanoTime();
+            try {
+                int resultRecursive = FibnacciRecursive.fibonacciRecursive(n);
+                long endTimeRecursive = System.nanoTime();
+                long durationRecursive = endTimeRecursive - startTimeRecursive;
+                System.out.println("Recursive Fibonacci(" + n + ") = " + resultRecursive + " | Time: " + durationRecursive / 1_000_000 + "ms");
+            } catch (StackOverflowError e) {
+                System.out.println("Recursive Fibonacci(" + n + ") is infeasible due to StackOverflow");
+            }
+
+
+            long startTimeIterative = System.nanoTime();
+            int resultIterative = FibnacciIterative.fibonacciIterative(n);
+            long endTimeIterative = System.nanoTime();
+            long durationIterative = endTimeIterative - startTimeIterative;
+            System.out.println("Iterative Fibonacci(" + n + ") = " + resultIterative + " | Time: " + durationIterative / 1_000_000 + "ms\n");
+        }
+    }
 }
